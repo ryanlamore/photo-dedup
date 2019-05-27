4746eff393bc8b187fde4ef0b1bc59b3c6862b13
Ryan Lamore <rlamore@salesforce.com>
2019-05-26 21:44:01 -0700

    WIP

--- src/main/java/us/sroysf/MatchAlgorithm.java
+++ src/main/java/us/sroysf/MatchAlgorithm.java
@@ -1,8 +1,32 @@
 package us.sroysf;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public enum MatchAlgorithm {
-    NameAndSize,
-    NameSizeAndMD5,
-    ExtensionSizeAndMD5,
+    NameAndSize {
+        @Override
+        String generateDuplicationKey(Path file) throws IOException {
+            return String.format("%s:%d", file.getFileName(), Files.size(file));
+        }
+    },
+    NameSizeAndMD5 {
+        @Override
+        String generateDuplicationKey(Path file) throws IOException {
+            return String.format("%s:%d:%s", file.getFileName(), Files.size(file), );
+        }
+    },
+    ExtensionSizeAndMD5 {
+        @Override
+        String generateDuplicationKey(Path file) throws IOException {
+            return null;
+        }
+    },
     ;
+
+    MatchAlgorithm() {}
+
+    abstract String generateDuplicationKey(Path fileInfo) throws IOException;
+
 }
